e0be18dc7f9a324c494d638e6398425caea928c2
ProgrammingtotheMax <leeimax.il@gmail.com>
2025-07-17 16:23:58 -0700

    Fixed Intake.java after merging it with swerve

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,15 +1,8 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.signals.InvertedValue;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.io.LoggedTalonFX;
 import frc.robot.io.TalonFXIO;
-
-
-
-
 
 public class Intake extends SubsystemBase{
     private LoggedTalonFX motor;
@@ -23,10 +16,7 @@ public class Intake extends SubsystemBase{
     }
 
     public Intake(TalonFXIO motorIO){
-        TalonFXConfiguration motorConfig = new TalonFXConfiguration();
-        motorConfig.MotorOutput.Inverted =
-                Constants.motorInverted ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
-        motor = new LoggedTalonFX(motorIO, "Intake/Motor", motorConfig);  
+        motor = new LoggedTalonFX(motorIO, "Intake/Motor");  
     }
 
     public void setSpeed(double value) {
