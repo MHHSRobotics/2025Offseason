5cf23938770f7373851d22aa8586abff1d34e848
npasetto0 <blasphemorgulus@gmail.com>
2025-07-31 11:44:41 -0700

    Added preliminary swerve code

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -1,5 +1,17 @@
 package frc.robot.subsystems.swerve;
 
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.io.GyroIO;
@@ -7,9 +19,64 @@ import frc.robot.io.GyroIO;
 public class Swerve extends SubsystemBase {
     public static class Constants {}
 
+    private GyroIO gyro;
     private SwerveModule[] modules = new SwerveModule[4];
 
-    public Swerve(GyroIO gyro, SwerveModule[] modules) {
-        this.modules = modules;
+    private Rotation2d gyroAngle=new Rotation2d();
+
+    private SwerveDriveKinematics kinematics=new SwerveDriveKinematics(getModuleTranslations());
+    private SwerveDrivePoseEstimator estimator=new SwerveDrivePoseEstimator(kinematics, gyroAngle, getWheelPositions(), new Pose2d());
+    public Swerve(GyroIO gyro, SwerveModule fl,SwerveModule fr,SwerveModule bl,SwerveModule br) {
+        this.gyro=gyro;
+        this.modules = new SwerveModule[]{fl,fr,bl,br};
+    }
+
+    public static Translation2d[] getModuleTranslations() {
+        return new Translation2d[] {
+            new Translation2d(TunerConstants.FrontLeft.LocationX, TunerConstants.FrontLeft.LocationY),
+            new Translation2d(TunerConstants.FrontRight.LocationX, TunerConstants.FrontRight.LocationY),
+            new Translation2d(TunerConstants.BackLeft.LocationX, TunerConstants.BackLeft.LocationY),
+            new Translation2d(TunerConstants.BackRight.LocationX, TunerConstants.BackRight.LocationY)
+        };
+    }
+
+    @AutoLogOutput(key = "Odometry/Robot")
+    public Pose2d getPose(){
+        return estimator.getEstimatedPosition();
+    }
+
+    public void setPose(Pose2d newPose){
+        estimator.resetPosition(gyroAngle, getWheelPositions(), newPose);
+    }
+
+    public SwerveModulePosition[] getWheelPositions(){
+        SwerveModulePosition[] states = new SwerveModulePosition[4];
+        for (int i = 0; i < 4; i++) {
+            states[i] = modules[i].getPosition();
+        }
+        return states;
+    }
+
+    public void runVelocity(ChassisSpeeds speeds){
+        ChassisSpeeds discreteSpeeds=ChassisSpeeds.discretize(speeds, 0.02);
+        SwerveModuleState[] setpointStates=kinematics.toSwerveModuleStates(discreteSpeeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(setpointStates, TunerConstants.kSpeedAt12Volts);
+        for (int i=0; i<4; i++) {
+            modules[i].runSetpoint(setpointStates[i]);
+        }
+    }
+
+    public void stop(){
+        runVelocity(new ChassisSpeeds());
+    }
+
+    @Override
+    public void periodic(){
+        for (SwerveModule module : modules) {
+            module.periodic();
+        }
+        gyro.updateInputs();
+        Logger.processInputs("Drive/Gyro", gyro.getInputs());
+        estimator.updateWithTime(RobotController.getFPGATime(), gyroAngle, getWheelPositions());
     }
 }
