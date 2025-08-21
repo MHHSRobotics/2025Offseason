9c38ceea783cb8a3aa7f638c0a79f5a53f7db95a
Maxwell-Delatorre <maxwelldelatorre2009@gmail.com>
2025-08-21 16:05:57 -0700

    Updated Hang

--- src/main/java/frc/robot/subsystems/Hang.java
+++ src/main/java/frc/robot/subsystems/Hang.java
@@ -1,3 +1,38 @@
 package frc.robot.subsystems;
 
-public class Hang {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+import frc.robot.io.CANcoderIO;
+import frc.robot.io.LoggedTalonFX;
+import frc.robot.io.TalonFXIO;
+
+public class Hang extends SubsystemBase {
+    public static class Constants {
+        public static final int encoderId = 3;
+        public static final double hangSpeed=1.0;
+        public static final int motorId = 25;
+        public static final boolean motorInverted = true;
+    }
+
+    private LoggedTalonFX motor;
+
+    public Hang(TalonFXIO motorIO, CANcoderIO encoderIO) {
+        TalonFXConfiguration motorConfig = new TalonFXConfiguration();
+        motor = new LoggedTalonFX(motorIO, "Hang/Motor", motorConfig);
+        motor.setInverted(Constants.motorInverted);
+    }
+
+    public void setSpeed(double value) {
+        motor.setSpeed(value);
+    }
+
+    public void stop() {
+        motor.setSpeed(0);
+    }
+
+    public void periodic() {
+        motor.periodic();
+    }
+}
