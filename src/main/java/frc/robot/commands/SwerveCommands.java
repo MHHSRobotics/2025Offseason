2a0a91e5f04a7024f1e9cf822d3ced1238bc0c5a
npasetto0 <blasphemorgulus@gmail.com>
2025-10-09 13:04:08 -0700

    Preliminary auto align code, refactored swerve

--- src/main/java/frc/robot/commands/SwerveCommands.java
+++ src/main/java/frc/robot/commands/SwerveCommands.java
@@ -17,12 +17,6 @@ public class SwerveCommands {
         this.swerve = swerve;
     }
 
-    // Stops the movement of the swerve drive
-    public Command stop() {
-        // return new InstantCommand(() -> swerve.stop(), swerve).withName("swerve stop");
-        return new InstantCommand();
-    }
-
     // Puts the swerve drive into an X position so it can't be pushed
     public Command lock() {
         return new InstantCommand(() -> swerve.lock(), swerve).withName("swerve lock");
@@ -66,7 +60,11 @@ public class SwerveCommands {
                             rotation = Math.copySign(rotationScale, rotation);
 
                             // Run the swerve drive with the given values of x, y, and rotation
-                            swerve.runSpeeds(x, y, rotation, fieldRelative.getAsBoolean());
+                            if (x != 0 || y != 0 || rotation != 0) {
+                                swerve.setPositionOutput(x, y);
+                                swerve.setRotationOutput(rotation);
+                                swerve.setFieldOriented(fieldRelative.getAsBoolean());
+                            }
                         },
                         swerve)
                 .withName("swerve drive");
