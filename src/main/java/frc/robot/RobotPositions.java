b33bcd880a8cf5a5735be1071008dd2bb1bca5cc
npasetto0 <blasphemorgulus@gmail.com>
2025-10-13 16:10:08 -0700

    Added superstructure

---
+++ src/main/java/frc/robot/RobotPositions.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+
+public class RobotPositions {
+    public static final double armDefault=Units.degreesToRadians(90);
+    public static final double elevatorDefault=0.05;
+    public static final double wristDefault=Units.degreesToRadians(90);
+
+    public static final double armL1=Units.degreesToRadians(0);
+    public static final double elevatorL1=0;
+    public static final double wristL1=Units.degreesToRadians(0);
+
+    public static final double armL2=Units.degreesToRadians(0);
+    public static final double elevatorL2=0;
+    public static final double wristL2=Units.degreesToRadians(0);
+
+    public static final double armL3=Units.degreesToRadians(0);
+    public static final double elevatorL3=0;
+    public static final double wristL3=Units.degreesToRadians(0);
+
+    public static final double armL4=Units.degreesToRadians(0);
+    public static final double elevatorL4=0;
+    public static final double wristL4=Units.degreesToRadians(0);
+
+    public static final double armLowAlgae=Units.degreesToRadians(0);
+    public static final double elevatorLowAlgae=0;
+    public static final double wristLowAlgae=Units.degreesToRadians(0);
+
+    public static final double armHighAlgae=Units.degreesToRadians(0);
+    public static final double elevatorHighAlgae=0;
+    public static final double wristHighAlgae=Units.degreesToRadians(0);
+
+    public static final double armSource=Units.degreesToRadians(0);
+    public static final double elevatorSource=0;
+    public static final double wristSource=Units.degreesToRadians(0);
+}
