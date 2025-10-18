9078d8730ac39a71af5401acd94248b8837e9eff
npasetto0 <blasphemorgulus@gmail.com>
2025-10-18 14:25:32 -0700

    Finished testing

--- src/main/java/frc/robot/RobotPositions.java
+++ src/main/java/frc/robot/RobotPositions.java
@@ -7,21 +7,21 @@ public class RobotPositions {
     public static final double elevatorDefault = 0.05;
     public static final double wristDefault = Units.degreesToRadians(90);
 
-    public static final double armL1 = Units.degreesToRadians(0);
-    public static final double elevatorL1 = 0;
-    public static final double wristL1 = Units.degreesToRadians(0);
+    public static final double armL1 = 2.06;
+    public static final double elevatorL1 = 0.1;
+    public static final double wristL1 = 0.22;
 
-    public static final double armL2 = Units.degreesToRadians(0);
-    public static final double elevatorL2 = 0;
-    public static final double wristL2 = Units.degreesToRadians(0);
+    public static final double armL2 = 2.25;
+    public static final double elevatorL2 = 0.3;
+    public static final double wristL2 = 0;
 
-    public static final double armL3 = Units.degreesToRadians(0);
-    public static final double elevatorL3 = 0;
-    public static final double wristL3 = Units.degreesToRadians(0);
+    public static final double armL3 = 2.25;
+    public static final double elevatorL3 = 0.76;
+    public static final double wristL3 = 0.23;
 
-    public static final double armL4 = Units.degreesToRadians(0);
-    public static final double elevatorL4 = 0;
-    public static final double wristL4 = Units.degreesToRadians(0);
+    public static final double armL4 = 2.3;
+    public static final double elevatorL4 = 1.1;
+    public static final double wristL4 = -0.44;
 
     public static final double armLowAlgae = Units.degreesToRadians(0);
     public static final double elevatorLowAlgae = 0;
@@ -31,7 +31,7 @@ public class RobotPositions {
     public static final double elevatorHighAlgae = 0;
     public static final double wristHighAlgae = Units.degreesToRadians(0);
 
-    public static final double armSource = Units.degreesToRadians(0);
-    public static final double elevatorSource = 0;
-    public static final double wristSource = Units.degreesToRadians(0);
+    public static final double armSource = Units.degreesToRadians(45);
+    public static final double elevatorSource = 0.5;
+    public static final double wristSource = Units.degreesToRadians(-90);
 }
