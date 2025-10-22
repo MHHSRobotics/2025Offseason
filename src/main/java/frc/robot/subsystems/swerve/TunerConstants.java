958be09d827c46740770fb7864c88b31e337f0e4
npasetto0 <blasphemorgulus@gmail.com>
2025-10-22 09:35:38 -0700

    Nerfed steer PID

--- src/main/java/frc/robot/subsystems/swerve/TunerConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/TunerConstants.java
@@ -18,7 +18,7 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-            .withKP(100)
+            .withKP(20)
             .withKI(0)
             .withKD(0)
             .withKS(0)
